46f6bd7918460f7c003215ca7382c72d0fccbb59
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2020-12-16 10:41:13 +0100

    Leer por teclado

--- proyecto-java/src/com/entornosdesarrollo/App.java
+++ proyecto-java/src/com/entornosdesarrollo/App.java
@@ -1,9 +1,19 @@
 package com.entornosdesarrollo;
 
+import java.util.Scanner;
+
 public class App {
 
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo!");
+	}
+	
+	
+	public static String leerStringPorConsola() {
+		Scanner sc = new Scanner(System.in);
+		String result = sc.next();
+		sc.close();
+		return result;
 	}
 
 }
